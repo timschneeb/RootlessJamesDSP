bc4db69e8c7c397e72791221a2024f1a0d87224b
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-07-30 19:08:37 +0200

    Replace libjamesdsp subtree with submodule

--- app/src/main/cpp/update_subtree.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(git rev-parse --show-toplevel)" || exit
-git subtree pull --prefix app/src/main/cpp/libjamesdsp-subtree https://github.com/james34602/JamesDSPManager.git master --squash
