542bf98fac890711ac40f9bb8cb178e558063909
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-08-01 00:05:29 +0200

    Misc changes

--- hidden-api-impl/src/main/java/me/timschneeberger/hiddenapi_impl/AudioSystem.java
+++ hidden-api-impl/src/main/java/me/timschneeberger/hiddenapi_impl/AudioSystem.java
@@ -1,48 +1,28 @@
 package me.timschneeberger.hiddenapi_impl;
 
+import android.annotation.SuppressLint;
+
 public class AudioSystem {
-
     private static final int REFLECTION_ERROR = -999;
+    private Class<?> mAudioSystem;
 
-    private static final String DEVICE_OUT_SPEAKER = "DEVICE_OUT_SPEAKER";
-    private static final String DEVICE_OUT_EARPIECE = "DEVICE_OUT_EARPIECE";
-    private static final String DEVICE_OUT_WIRED_HEADPHONE = "DEVICE_OUT_WIRED_HEADPHONE";
-
-
-    Class<?> mAudioSystem;
-
+    @SuppressLint("PrivateApi")
     public AudioSystem() {
-
         try {
             mAudioSystem = Class.forName("android.media.AudioSystem");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
-    }
-
-    private int getConstantValue(String s) {
-
-        try {
-            return ((Integer) mAudioSystem.getDeclaredField(s).get(int.class)).intValue();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return REFLECTION_ERROR;
-
     }
 
 
+    @SuppressWarnings("ConstantConditions")
     public int setAllowedCapturePolicy(int uid, int flags) {
-
         try {
             return (Integer) mAudioSystem.getMethod("setAllowedCapturePolicy", int.class, int.class).invoke(mAudioSystem, uid, flags);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return REFLECTION_ERROR;
+        return -1;
     }
-
 }
