b2ce47785a5bb580257f5758f8b6fda05eed3187
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-10-21 00:49:48 +0200

    feat(root): add UI for library load failure

---
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/fragment/LibraryLoadErrorFragment.kt
@@ -0,0 +1,25 @@
+package me.timschneeberger.rootlessjamesdsp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.timschneeberger.rootlessjamesdsp.databinding.FragmentLibraryLoadErrorBinding
+import me.timschneeberger.rootlessjamesdsp.utils.ContextExtensions.openPlayStoreApp
+
+class LibraryLoadErrorFragment : Fragment() {
+    private lateinit var binding: FragmentLibraryLoadErrorBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentLibraryLoadErrorBinding.inflate(layoutInflater, container, false)
+        binding.rootlessNotice.setOnClickListener {
+            requireContext().openPlayStoreApp("me.timschneeberger.rootlessjamesdsp")
+        }
+        return binding.root
+    }
+}
