78fe5ed5a558b91c592225d6a34d25836b39207e
Tim Schneeberger <tim.schneeberger@outlook.de>
2023-03-09 01:31:05 +0100

    refactor: use sdkAbove helper in CompatExtensions

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/utils/extensions/CompatExtensions.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/utils/extensions/CompatExtensions.kt
@@ -6,49 +6,49 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import me.timschneeberger.rootlessjamesdsp.utils.sdkAbove
 import java.io.Serializable
 
+@Suppress("DEPRECATION")
 object CompatExtensions {
     @Suppress("UNCHECKED_CAST")
     fun <T : Serializable> Bundle.getSerializableAs(key: String, clazz: Class<T>): T? {
-        return (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        return sdkAbove(Build.VERSION_CODES.TIRAMISU) {
             this.getSerializable(key, clazz)
-        } else {
-            @Suppress("DEPRECATION")
-            this.getSerializable(key)
-        }) as? T
+        }.below {
+            this.getSerializable(key) as? T
+        }
     }
 
     inline fun <reified T : Parcelable> Bundle.getParcelableAs(key: String): T? {
-        return (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        return sdkAbove(Build.VERSION_CODES.TIRAMISU) {
             this.getParcelable(key, T::class.java)
-        } else {
-            @Suppress("DEPRECATION")
+        }.below {
             this.getParcelable(key)
-        })
+        }
     }
 
     fun PackageManager.getPackageInfoCompat(packageName: String, flags: Int = 0): PackageInfo {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        return sdkAbove(Build.VERSION_CODES.TIRAMISU) {
             getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(flags.toLong()))
-        } else {
-            @Suppress("DEPRECATION") getPackageInfo(packageName, flags)
+        }.below {
+            getPackageInfo(packageName, flags)
         }
     }
 
     fun PackageManager.getApplicationInfoCompat(packageName: String, flags: Int = 0): ApplicationInfo {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        return sdkAbove(Build.VERSION_CODES.TIRAMISU) {
             getApplicationInfo(packageName, PackageManager.ApplicationInfoFlags.of(flags.toLong()))
-        } else {
-            @Suppress("DEPRECATION") getApplicationInfo(packageName, flags)
+        }.below {
+            getApplicationInfo(packageName, flags)
         }
     }
 
     fun PackageManager.getInstalledApplicationsCompat(flags: Int = 0): List<ApplicationInfo> {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        return sdkAbove(Build.VERSION_CODES.TIRAMISU) {
             getInstalledApplications(PackageManager.ApplicationInfoFlags.of(flags.toLong()))
-        } else {
-            @Suppress("DEPRECATION") getInstalledApplications(flags)
+        }.below {
+            getInstalledApplications(flags)
         }
     }
 }
