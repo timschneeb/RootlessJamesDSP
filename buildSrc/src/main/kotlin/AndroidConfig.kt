2cee2af3156d138d31bc5a0986a22e63e0abcf43
Tim Schneeberger <tim.schneeberger@outlook.de>
2023-03-10 17:00:43 +0100

    chore: bump version

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -4,8 +4,8 @@ object AndroidConfig {
     const val compileSdk = 33
     const val targetSdk = 33
 
-    const val versionName = "1.3.2"
-    const val versionCode = 26
+    const val versionName = "1.4.0"
+    const val versionCode = 27
 
     const val kotlinVersion = "1.8.10"
     const val kspVersion = "1.8.10-1.0.9"
