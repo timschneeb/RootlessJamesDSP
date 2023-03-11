6844df98874042a4984d02c25ef427d075a31f88
Tim Schneeberger <tim.schneeberger@outlook.de>
2023-03-11 20:48:03 +0100

    fix: rollback refine plugin

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -4,11 +4,11 @@ object AndroidConfig {
     const val compileSdk = 33
     const val targetSdk = 33
 
-    const val versionName = "1.4.0"
-    const val versionCode = 27
+    const val versionName = "1.4.1"
+    const val versionCode = 28
 
     const val kotlinVersion = "1.8.10"
     const val kspVersion = "1.8.10-1.0.9"
     const val shizukuVersion = "12.2.0"
-    const val rikkaRefineVersion = "4.1.0"
+    const val rikkaRefineVersion = "3.1.1"
 }
