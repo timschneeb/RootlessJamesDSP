3500a1e3025e3c8aacd327aa0ef61a44f130bbc1
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-12-12 18:47:06 +0100

    chore: bump version & add changelog

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -4,8 +4,8 @@ object AndroidConfig {
     const val compileSdk = 35
     const val targetSdk = 34
 
-    const val versionName = "1.6.9"
-    const val versionCode = 46
+    const val versionName = "1.6.10"
+    const val versionCode = 47
 
     const val kotlinVersion = "2.1.0"
     const val kspVersion = "2.1.0-1.0.29"
