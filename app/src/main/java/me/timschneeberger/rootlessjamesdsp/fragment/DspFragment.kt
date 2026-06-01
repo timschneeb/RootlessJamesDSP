bac4805ae12a96a2be7c4c7c473007cd7a9ffaf2
nfxproj <nfxproj@gmail.com>
2026-06-01 02:41:37 +0200

    Hide disabled DSP modules from user interface

--- app/src/main/java/me/timschneeberger/rootlessjamesdsp/fragment/DspFragment.kt
+++ app/src/main/java/me/timschneeberger/rootlessjamesdsp/fragment/DspFragment.kt
@@ -77,14 +77,6 @@ class DspFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListen
                     R.xml.dsp_output_control_preferences
                 ))
             .replace(
-                R.id.card_compressor, PreferenceGroupFragment.newInstance(Constants.PREF_COMPANDER,
-                    R.xml.dsp_compander_preferences
-                ))
-            .replace(
-                R.id.card_bass, PreferenceGroupFragment.newInstance(Constants.PREF_BASS,
-                    R.xml.dsp_bass_preferences
-                ))
-            .replace(
                 R.id.card_eq, PreferenceGroupFragment.newInstance(Constants.PREF_EQ,
                     R.xml.dsp_equalizer_preferences
                 ))
@@ -95,14 +87,6 @@ class DspFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListen
             .replace(
                 R.id.card_geq, PreferenceGroupFragment.newInstance(Constants.PREF_GEQ,
                     R.xml.dsp_graphiceq_preferences
-                ))
-            .replace(
-                R.id.card_ddc, PreferenceGroupFragment.newInstance(Constants.PREF_DDC,
-                    R.xml.dsp_ddc_preferences
-                ))
-            .replace(
-                R.id.card_convolver, PreferenceGroupFragment.newInstance(Constants.PREF_CONVOLVER,
-                    R.xml.dsp_convolver_preferences
                 ))
             .replace(
                 R.id.card_liveprog, PreferenceGroupFragment.newInstance(Constants.PREF_LIVEPROG,
@@ -116,15 +100,19 @@ class DspFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListen
                 R.id.card_stereowide, PreferenceGroupFragment.newInstance(Constants.PREF_STEREOWIDE,
                     R.xml.dsp_stereowide_preferences
                 ))
-            .replace(
-                R.id.card_crossfeed, PreferenceGroupFragment.newInstance(Constants.PREF_CROSSFEED,
-                    R.xml.dsp_crossfeed_preferences
-                ))
-            .replace(
-                R.id.card_reverb, PreferenceGroupFragment.newInstance(Constants.PREF_REVERB,
-                    R.xml.dsp_reverb_preferences
-                ))
             .commit()
+
+        // Hide disabled modules
+        listOf(
+            binding.cardCompressor,
+            binding.cardBass,
+            binding.cardDdc,
+            binding.cardConvolver,
+            binding.cardCrossfeed,
+            binding.cardReverb
+        ).forEach {
+            (it.parent as? View)?.isVisible = false
+        }
 
         // Load initial preferences
         arrayOf(R.string.key_device_profiles_enable).forEach {
